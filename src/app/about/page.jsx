352a1090041ba1f016cda52ae0aa9403be0a7a90
Adil Iqbal <adiliqbal@Adils-MacBook-Pro.local>
2023-07-27 08:08:37 -0300

    Add About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+export default function About(){
+    return(
+        <div className="mx-auto max-w-6xl space-y-4">
+            <h1 className="text-2xl font-medium text-amber-600">About</h1>
+            <p>Enter details here....</p>
+            <p>Enter details here....</p>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <h1>Home Page</h1>
+    <div className="mx-auto max-w-6xl space-y-4">
+    <h1 className="text-2xl font-medium text-amber-600">Home Page</h1>
+    <p>Enter details here....</p>
+    <p>Enter details here....</p>
+    </div>
   )
 }
