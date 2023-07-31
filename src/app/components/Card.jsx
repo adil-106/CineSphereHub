9b45df69db701f62b08a522622fcfdaf4cf175cb
Adil Iqbal <adiliqbal@Adils-MacBook-Pro.local>
2023-07-31 11:57:40 -0300

    Retrieve TMDB results in Card Component

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+import {AiFillLike} from "react-icons/ai"
+
+export default function Card({result}){
+    return(<div className="cursor-pointer p-3 hover:shadow-slate-400 shadow-md rounded-lg border border-slate-400 m-2 transition-shadow duration-200 group">
+            <Link href={`/movie/${result.id}`}>
+                <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} style={{maxWidth:"100%",height:"auto"}} width={500} height={300} className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200" placeholder="blur" blurDataURL="/next.svg" alt="Image Not Found"></Image>
+                <div className="p-2">
+                    <p className="line-clamp-2 text-md">{result.overview}</p>
+                    <h2 className="truncate text-lg font-bold">{result.original_title}</h2>
+                    <p className="flex items-center">{result.release_date}<AiFillLike className="h-5 mr-1 ml-3"/>{result.vote_count}</p>
+                </div>
+            </Link>
+        </div>);
+}
--- src/app/components/Results.jsx
+++ src/app/components/Results.jsx
@@ -1,11 +1,12 @@
+import Card from "./Card";
+
 export default function Results({results}){
+    console.log(results);
     return(
-        <div>
+        <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4 px-4">
             {results.map((result) => {
-                return(<div key={result.id}>
-                    {result.original_title}
-                    </div>); 
+                return <Card key={result.id} result={result}/>
             })}
         </div>
-    )
+    );
 }
