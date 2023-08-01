a51847b23d4ed2a755e1025d597caafa2793fe58
Adil Iqbal <adiliqbal@Adils-MacBook-Pro.local>
2023-08-01 11:04:12 -0300

    Modify About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 export default function About(){
     return(
         <div className="mx-auto max-w-6xl space-y-4 px-6">
             <h1 className="text-2xl font-medium text-amber-500">About</h1>
-            <p>Enter details here....</p>
-            <p>Enter details here....</p>
+            <p>This is a movie database website based on React.js and its framework NextJS. The UI is built with the use of TailwindCSS a CSS framework. 
+                The website consits of various functionalities including dark and ligit mode switching, search capabilites. The website switches dynamically between client side rendering whenever it makes the usage of React hooks and server side rendering.
+                This also incorporates the use of react icons, react themes and next-navigation ~ react router for fast and robust rendering ~ Created by <Link className="font-semibold underline" href="https://www.linkedin.com/in/adil-iqbal-ai">Adil Iqbal.</Link>
+            </p>
+            
         </div>
     )
 }
