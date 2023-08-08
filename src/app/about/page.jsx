a5837f7b6a2e9942a03cfd22ed6dc08f71211942
Adil Iqbal <adiliqbal@Adils-MacBook-Pro.local>
2023-08-08 10:54:31 -0300

    Modified About Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,10 +2,9 @@ import Link from "next/link";
 export default function About(){
     return(
         <div className="mx-auto max-w-6xl space-y-4 px-6">
-            <h1 className="text-2xl font-medium text-amber-500">About</h1>
-            <p>This is a movie database website based on React.js and its framework NextJS. The UI is built with the use of TailwindCSS a CSS framework. 
-                The website consits of various functionalities including dark and ligit mode switching, search capabilites. The website switches dynamically between client side rendering whenever it makes the usage of React hooks and server side rendering.
-                This also incorporates the use of react icons, react themes and next-navigation ~ react router for fast and robust rendering ~ Created by <Link className="font-semibold underline" href="https://www.linkedin.com/in/adil-iqbal-ai">Adil Iqbal.</Link>
+            <h1 className="text-2xl font-medium text-amber-500 ">About</h1>
+            <p className="text-justify">
+                This is a movie database website which resembles IMDb. This responsive web application is built using principles of ReactJS incorporating a component-based architecture and utilizing a web-framework based on React named NextJS. TailwindCSS is utilized to build a compelling UI thereby, adding interactive and responsiveness. The website consits of various functionalities including dark and ligit mode switching, search capabilites which are built using React Hooks such as useState, useRouter and useEffect. The website switches dynamically between client side rendering (CSR) whenever it uses useContext and Server Side rendering(SSR). This also incorporates the use of react-icons, react-themes, and next-navigation for fast and robust page rendering.  ~ Created by <Link className="font-semibold underline text-amber-500 hover:text-gray-50" href="https://www.linkedin.com/in/adil-iqbal-ai">Adil Iqbal.</Link>
             </p>
             
         </div>
